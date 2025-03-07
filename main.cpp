4e1154f3ec8c72b90660cd28475eb6f3f8ef19ec
Prabuddhshetty901 <prabuddhshetty916@gmail.com>
2025-03-07 22:33:39 +0530

    - Reduced code - Fix Bird and Pipe movement - Add random pipe height

--- main.cpp
+++ main.cpp
@@ -8,23 +8,27 @@ void s_Update();
 void s_Draw();
 
 void p_Draw();
+void p_Update();
 
 
-int s_width = 1080;
+int s_width = 600;
 int s_height = 700;
 
 
-Rectangle s = {20,s_height / 2 - 20 ,20,20};
+Rectangle s = {200,s_height / 2 - 20 ,20,20};
 float velocity = 0;
 
 
 
-Rectangle p1 = { 1 * s_width / 4, 0, 60, 250 };
-Rectangle p2 = { 1 * s_width / 4, s_height - 250, 60, 250 };
-Rectangle p3 = { 2 * s_width / 4, 0, 60, 250 };
-Rectangle p4 = { 2 * s_width / 4, s_height - 250, 60, 250 };
-Rectangle p5 = { 3 * s_width / 4, 0, 60, 250 };
-Rectangle p6 = { 3 * s_width / 4, s_height - 250, 60, 250 };
+float pipeSpeed = 3.0f;
+float pipeGap = 150; // constant Gap between top and bottom pipes
+
+Rectangle pipes[4] = {
+	{s_width, 0, 60, 250},               
+	{s_width, 250 + pipeGap, 60, s_height - 250 + pipeGap},
+	{s_width + (s_width / 2), 0, 60, 150},   
+	{s_width + (s_width / 2), 150 + pipeGap, 60, s_height - 150 + pipeGap}
+};
 
 int main() {
 
@@ -47,6 +51,7 @@ int main() {
 		s_Draw();
 		s_Update();
 		p_Draw();
+		p_Update();
 		
 		/*cout << "s_x: " << s_x << " s_y :" << s_y << endl;*/
 		
@@ -61,18 +66,19 @@ int main() {
 
 void s_Draw() {
 	DrawRectangle(s.x, s.y, s.width, s.height, WHITE);
-	s.x++;
 }
 
 
 void s_Update() {
-	float gravity = 0.5;
-	float jumpStrength = -8.0;
+	float gravity = 1;
+	float jumpStrength = -14.0;
 
-	if (CheckCollisionRecs(s, p1) || CheckCollisionRecs(s, p2) || CheckCollisionRecs(s, p3) || CheckCollisionRecs(s, p4) ||
-		CheckCollisionRecs(s, p5) || CheckCollisionRecs(s, p6) || s.x> s_width - s.width){
-		s.x = 20;
-		s.y = s_height / 2 - 20;
+	for (int i = 0; i < 4; i++) {
+		if (CheckCollisionRecs(s, pipes[i])) {
+			s.x = 200;
+			s.y = s_height / 2 - 20;
+			velocity = 0;
+		}
 	}
 
 
@@ -97,13 +103,26 @@ void s_Update() {
 void p_Draw() {
 
 
-	DrawRectangle(p1.x,p1.y,p1.width,p1.height, RED);
-	DrawRectangle(p2.x, p2.y, p2.width, p2.height, RED);
+	for (int i = 0; i < 4; i++) {
+		DrawRectangle(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height, RED);
+	}
+}
 
-	DrawRectangle(p3.x, p3.y, p3.width, p3.height, RED);
-	DrawRectangle(p4.x, p4.y, p4.width, p4.height, RED);
+void p_Update() {
+
+	for (int i = 0; i < 4; i += 2) {  // alternative pipes
+		pipes[i].x -= pipeSpeed;      //top pipe
+		pipes[i + 1].x -= pipeSpeed;  //bottom pipe
+
+		if (pipes[i].x + pipes[i].width < 0) { // resetting pipes when they go off screen
+			pipes[i].x = s_width;     // Reset top pipe -> width
+			pipes[i + 1].x = s_width; // Reset bottom pipe -> width
 
 
-	DrawRectangle(p5.x, p5.y, p5.width, p5.height, RED);
-	DrawRectangle(p6.x, p6.y, p6.width, p6.height, RED);
+			int newHeight = GetRandomValue(100, 400); // Varry pipe height
+			pipes[i].height = newHeight; //top pipe -. height
+			pipes[i + 1].y = newHeight + pipeGap; // bottom pipe(y pos -> Maintaining gap)
+			pipes[i + 1].height = s_height - (newHeight + pipeGap); // bottom pipe -> height
+		}
+	}
 }
