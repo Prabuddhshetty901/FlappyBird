b5ee3c648ba3e67a587385a85546e8590cbac896
Prabuddhshetty901 <prabuddhshetty916@gmail.com>
2025-03-16 19:35:32 +0530

    - Completed game logic - Changed color

--- main.cpp
+++ main.cpp
@@ -15,7 +15,9 @@ void ResetPage();
 
 
 int s_width = 600;
-int s_height = 700;
+int s_height = 800;
+int offset = 45;
+int groundHeight = 50;
 
 
 Rectangle s = { 200,s_height / 2 - 20 ,20,20 };
@@ -25,23 +27,24 @@ float rotation = 0;  // Rotation angle
 
 
 int score = 0;
-bool passedPipe[2] = { false, false };  // Track whether a pipe pair has been counted
-int winscore = 5;
+// Track whether a pipe pair has been counted
+bool passedPipe[2] = { false, false };  
+int winscore = 50;
 int highScore = 0;
 
 
 float pipeSpeed = 3.0f;
-float pipeGap = 150; // constant Gap between top and bottom pipes
+float pipeGap = 250; // constant Gap between top and bottom pipes
 
 
 bool gameover = false;
 
 
 Rectangle pipes[4] = {
-	{s_width, 0, 60, 250},
-	{s_width, 250 + pipeGap, 60, s_height - 250 + pipeGap},
-	{s_width + (s_width / 2), 0, 60, 150},
-	{s_width + (s_width / 2), 150 + pipeGap, 60, s_height - 150 + pipeGap}
+	{s_width, 50, 60, 250},
+	{s_width, 250 + pipeGap, 60, s_height -groundHeight - (250+ pipeGap)},
+	{s_width + (s_width / 2), 50, 60, 150},
+	{s_width + (s_width / 2), 150 + pipeGap, 60, s_height - groundHeight -(150+ pipeGap)}
 };
 
 
@@ -64,7 +67,7 @@ int main() {
 
 
 		//clearing
-		ClearBackground(lightGrey);
+		ClearBackground(darkGrey);
 
 		if (!gameover) {
 
@@ -97,9 +100,13 @@ void s_Draw() {
 
 	Vector2 pivot = { s.width / 2, s.height / 2 };
 
-	DrawRectanglePro(s, pivot, rotation, darkGrey);
+	DrawRectanglePro(s, pivot, rotation, WHITE);
 
-	DrawText(TextFormat("Score: %d", score), 10, 10, 20, YELLOW);
+	DrawRectangle(0,45,s_width,5, lightGrey);
+	DrawRectangle(0, s_height-50, s_width, 5, lightGrey);
+
+	DrawText(TextFormat("Score: %d", score), 10, 10, 20, WHITE);
+	DrawText(TextFormat("High Score: %d", highScore), s_width - MeasureText("High Score: %d", 20) - 10, 10, 20, WHITE);
 }
 
 
@@ -109,7 +116,7 @@ void s_Update() {
 	float rotationSpeed = 5.0f; // Speed of rotation transition
 
 	for (int i = 0; i < 4; i++) {
-		if (CheckCollisionRecs(s, pipes[i])) {
+		if (CheckCollisionRecs(s, pipes[i]) || score == winscore) {
 			gameover = true;
 		}
 	}
@@ -122,15 +129,10 @@ void s_Update() {
 	velocity += gravity;
 	s.y += velocity;
 
-	if (s.y < 0) {
-		s.y = 0;
-		velocity = 0;
+	if ((s.y < 0) || s.y > s_height - 20 - groundHeight) {
+		gameover = true;
 	}
 
-	if (s.y > s_height - 20) {
-		s.y = s_height - 20;
-		velocity = 0;
-	}
 
 	if (velocity < 0) {
 		rotation -= rotationSpeed;
@@ -146,7 +148,7 @@ void p_Draw() {
 
 
 	for (int i = 0; i < 4; i++) {
-		DrawRectangle(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height, grey);
+		DrawRectangle(pipes[i].x, pipes[i].y, pipes[i].width, pipes[i].height, LIGHTGRAY);
 	}
 }
 
@@ -169,10 +171,10 @@ void p_Update() {
 			pipes[i + 1].x = s_width; // Reset bottom pipe -> width
 
 
-			int newHeight = GetRandomValue(100, 400); // Varry pipe height
+			int newHeight = GetRandomValue(100, 350); // Varry pipe height
 			pipes[i].height = newHeight; //top pipe -. height
 			pipes[i + 1].y = newHeight + pipeGap; // bottom pipe(y pos -> Maintaining gap)
-			pipes[i + 1].height = s_height - (newHeight + pipeGap); // bottom pipe -> height
+			pipes[i + 1].height = s_height - groundHeight - (newHeight + pipeGap); // bottom pipe -> height
 
 
 			passedPipe[i / 2] = false;  // Reset passed status for new pipes
@@ -187,37 +189,40 @@ void Restart() {
 
 
 
-	pipes[0] = { (float)s_width, 0, 60, 250 };
-	pipes[1] = { (float)s_width, 250 + pipeGap, 60, s_height - 250 + pipeGap };
-	pipes[2] = { (float)s_width + (s_width / 2), 0, 60, 150 };
-	pipes[3] = { (float)s_width + (s_width / 2), 150 + pipeGap, 60, s_height - 150 + pipeGap };
+	pipes[0] = { (float)s_width, 50, 60, 250 };
+	pipes[1] = { (float)s_width, 250 + pipeGap , 60, s_height - groundHeight - (250 + pipeGap) };
+	pipes[2] = { (float)s_width + (s_width / 2), 50, 60, 150 };
+	pipes[3] = { (float)s_width + (s_width / 2), 150 + pipeGap, 60, s_height - groundHeight - (150 + pipeGap) };
 }
 
 const char* w_text = "YOU WON";
 const char* r_text = "Press R to restart";
-const char* s_text = "Score: ";
-const char* h_text = "High Score: ";
-const char* l_text = "YOU LOSE";
+const char* l_text = "GAME OVER";
 
 
 void ResetPage() {
 	if (score == winscore) {
-		DrawText(w_text, ((s_width * s_height) - MeasureText(w_text, 60)) / 2, ((s_width * s_height) - MeasureText(w_text, 60)) / 2, 60, YELLOW);
-		DrawText(r_text, ((s_width * s_height) - MeasureText(r_text, 30)) / 2, ((s_width * s_height) - MeasureText(r_text, 30)) / 2 + 130, 30, YELLOW);
+		DrawText(w_text, (s_width - MeasureText(w_text, 60)) / 2, (s_height - MeasureText(w_text, 60)) / 2, 60, WHITE);
+		DrawText(r_text, (s_width - MeasureText(r_text, 30)) / 2, (s_height - MeasureText(r_text, 30)) / 2 + 130, 30, WHITE);
 
-		DrawText(TextFormat(s_text, score), ((s_width * s_height) - MeasureText(s_text, 30)) / 2, ((s_width * s_height) - MeasureText(s_text, 30)) / 2 + 100, 30, YELLOW);
-		DrawText(TextFormat(h_text, highScore), ((s_width * s_height) - MeasureText(h_text, 30)) / 2, ((s_width * s_height) - MeasureText(h_text, 30)) / 2 + 200, 30, YELLOW);
+		DrawText(TextFormat("Score: %d", score), (s_width - MeasureText(TextFormat("Score: %d", score), 30)) / 2, (s_height - MeasureText("Score: %d", 30)) / 2 + 100, 30, WHITE);
+		DrawText(TextFormat("High Score: %d", highScore), (s_width - MeasureText("High Score: %d", 30)) / 2, (s_height - MeasureText(TextFormat("High Score: %d", highScore), 30)) / 2 + 200, 30, WHITE);
 	}
 	else {
-		DrawText(l_text, ((s_width * s_height) - MeasureText(l_text, 60)) / 2, ((s_width * s_height) - MeasureText(l_text, 60)) / 2, 60, YELLOW);
-		DrawText(r_text, ((s_width * s_height) - MeasureText(r_text, 30)) / 2, ((s_width * s_height) - MeasureText(r_text, 30)) / 2 + 100, 30, YELLOW);
+		DrawText(l_text, (s_width - MeasureText(l_text, 60)) / 2, (s_height - MeasureText(l_text, 60)) / 2, 60, WHITE);
+		DrawText(r_text, (s_width - MeasureText(r_text, 30)) / 2, (s_height - MeasureText(r_text, 30)) / 2 + 100, 30, WHITE);
 
 
-		DrawText(TextFormat(s_text, score), ((s_width * s_height) - MeasureText(s_text, 30)) / 2, ((s_width * s_height) - MeasureText(s_text, 30)) / 2 + 100, 30, YELLOW);
-		DrawText(TextFormat(h_text, highScore), ((s_width * s_height) - MeasureText(h_text, 30)) / 2, ((s_width * s_height) - MeasureText(h_text, 30)) / 2 + 200, 30, YELLOW);
+		DrawText(TextFormat("Score: %d", score), (s_width - MeasureText(TextFormat("Score: %d", score), 30)) / 2, (s_height - MeasureText("Score: %d", 30)) / 2 + 100, 30, WHITE);
+		DrawText(TextFormat("High Score: %d", highScore), (s_width - MeasureText("High Score: %d", 30)) / 2, (s_height - MeasureText(TextFormat("High Score: %d", highScore), 30)) / 2 + 200, 30, WHITE);
 	}
 
+
 	if (IsKeyPressed(KEY_R)) {
+		if (score > highScore) {
+			highScore = score;
+		}
+
 		Restart();
 		score = 0;
 		gameover = false;
